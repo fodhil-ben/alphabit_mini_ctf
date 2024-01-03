4fa5037ac6b9c8cea4c993a858467c5e1bc077e9
Abderrahmen <abdouolicastro@gmail.com>
2024-01-03 22:30:05 +0100

    files fix

---
+++ rev/dungeon-2/solution/find.py
@@ -0,0 +1,28 @@
+hashed_array = ["h2O1BoGbiTlK","nFZE4r0pn42e","aC8OE4p4rKbN","jKOcv6yM0Zkq","RalOAb_teq8A","xlwgsxck75hX","ybrZYnP4Y57l","hLsYbjoKQMwY","ZacuWytt1aQQ","yhBXytONg2GY","VF331E_vKFD7","lp8bRG6W1CZz","bM8L4DS7UOo6","ae1ebFGv4WAE","psFRVyJlUCar","ZQmD2rFqnGoU","bGoIjliMIx4I","brZ4nrGO91Jg","EYsanIwCwmvW","okGxeEPKJZhL","YF8sA0uJq0ZS","IVxrhPbPHfjB","JOIJnUuVB3Ac","BIUg6EhyqbgG","OEUgDQiGBuxQ","hJ34AmXOGbDF","uytkTLogKRnT","SWTL6DLusYAn","Hg86ugkuLmx3","iurHZFGWPbBw","fhfV3kvhOjOk","Za_cKLzA_d7k","mGo2a824ML83","NWNhFFmXWim9","j55EzgtE9Mce","E5bnfLmSmrcI","HErLCBGsgE0G","BdpNd7XYlA65","j2PWTFz91OuR","C7iFhu3BBXgx","iF3Nq92DRPGR","l90HLImNArxx","wcX1C83CVEey","CIEM4oUxM5tn","iWmshpfraB3C","vCV_4nrHcs_c","yVK94cB6esv7","RWcnnx8GuAyU","rgfJs55k_QXT","E6h1X9QBZCGA","tr3O1SYUs0xS","UWk7vxOWLi4G","MfiPthtA_9Hi","b7_OJY5AhsTJ","k8b8l_GumbIh","v2suXR4hzTSw","BY7vlxmRncTL","zoR6TyPtvpbn","AsCqMIqsr6QD","vVRfaGfR1x9v","eKUHaMYV456T","OAiZI4hZrQKq","uW1XDH0NvEIQ","VPTXA03WRHn5","GW0qWRyW6QNN","P_3spTwmV36g","ptyAdybVwx0_","a0DIlA9itC4B","vOAy6MdK3GPU","wegkkCdMYHd4","KP7OnxCFNCu2","v0HqVC2CtdR7","KYI3_qf0UfAF","h_12sEjMtB_I","NovJUb7cbVi4","wRJOEGW4dZPe","mNACGj8KOmBJ","TM43eQBnwyAH","D7VJCgoeWr1M","A9vbUsq9fu_V","n5dvvpyjOVPO","bYCYGypG2i1C","kSg4itZp43c1","_1C7LV1uBBrE","R8KLZt1872oq","YQAnL6wpdyM0","hWX2qjlIc6MH","_1U9XRN8U16s","VXQKi9Y4B5Jx","c5hduFz2MlRv","i5JroO5On7J0","vcpZCHuqFUwO","c7GuwAzh0b6H","dUo6UnRFylH8","qW9iFKaj1x2N","YDbcSeMy7vTd","Wc6x7iDJ6Qof","qcXZLn9DD9Zo","iJ_Hx0s_tq_r","bAfGnSGXOcjh","B2LXeCSMX7gE","g9wkYTs2oGJU","oqu7WJ6_CQJY","GrIemsi5djUF","JvbtVWPi2m7O","p3ezfnjICF05","r9JGGJJoyjpk","wO8_UTU13uP1","U_Nd8u9XrO0Y","pIBib_4GB5KP","yQPok1wHyQey","otjlcvX3QlpV","HHC1tdE4FN6o","Gzsj1vlOQ72Q","YvYlem6l5YOG","rUBXCQ_Fur65","blt2YVfWXwku","vKNXMCkawalt","IziAtBlAXU54","LE_iOy_xxyz8","c49gACRy90MI","dBLZVL27FsLF","iHG9CCYGH7hs","CSdWRPm5YfKJ","UomhUdhuAvds","TTZhUEK7w3Rm","v9AUKPmwYda3","jJGXYCsOuf45","Wc78CNxvmd0W","mV2zICvU9vtT","UlCs4od9RLx0","WKYANFop_KMp","M5gan9aWHqcg","X6jXujOMJpxz","UKVdQ1Wr1ly0","_COToVykH4SC","X9_iFAwuAjrh","LIhEUnQxRJz0","X8XYkNcWebES","WGYqoL5OqvjL","A5di3hL7ihur","BTlb6ti9d3YD","Z6l025B7QYCJ","JQkncxKd46q2","V1f7uTrZ6RbV","qeNSEPOquUvz","MRwYJ26hEMfU","W3SeljGA7gjt","cFbq5aIky1vd","MKLdwTjSbT9R","bnjQhparWjQt","fEt0sblHO4YQ","A8IN5ni8vbep","rykM95jte6Xo","uiu0Rzbp_Zgo","Dub3nvhGECfR","w6xxWfrZLgYJ","a4lxc4DWVUKs","wR2ujf1PPAnL","mNACGj8KOmBJ","JMy1tfssTlir","D7VJCgoeWr1M","A9vbUsq9fu_V","n5dvvpyjOVPO","bYCYGypG2i1C"]
+functions = ["ArithmeticError","AssertionError","AttributeError","BaseException","BaseExceptionGroup","BlockingIOError","BrokenPipeError","BufferError","BytesWarning","ChildProcessError","ConnectionAbortedError","ConnectionError","ConnectionRefusedError","ConnectionResetError","DeprecationWarning","EOFError","Ellipsis","EncodingWarning","EnvironmentError","Exception","ExceptionGroup","False","FileExistsError","FileNotFoundError","FloatingPointError","FutureWarning","GeneratorExit","IOError","ImportError","ImportWarning","IndentationError","IndexError","InterruptedError","IsADirectoryError","KeyError","KeyboardInterrupt","LookupError","MemoryError","ModuleNotFoundError","NameError","None","NotADirectoryError","NotImplemented","NotImplementedError","OSError","OverflowError","PendingDeprecationWarning","PermissionError","ProcessLookupError","RecursionError","ReferenceError","ResourceWarning","RuntimeError","RuntimeWarning","StopAsyncIteration","StopIteration","SyntaxError","SyntaxWarning","SystemError","SystemExit","TabError","TimeoutError","True","TypeError","UnboundLocalError","UnicodeDecodeError","UnicodeEncodeError","UnicodeError","UnicodeTranslateError","UnicodeWarning","UserWarning","ValueError","Warning","ZeroDivisionError","__build_class__","__debug__","__doc__","__import__","__loader__","__name__","__package__","__spec__","abs","aiter","all","anext","any","ascii","bin","bool","breakpoint","bytearray","bytes","callable","chr","classmethod","compile","complex","copyright","credits","delattr","dict","dir","divmod","enumerate","eval","exec","exit","filter","float","format","frozenset","getattr","globals","hasattr","hash","help","hex","id","input","int","isinstance","issubclass","iter","len","license","list","locals","map","max","memoryview",min,"next","object","oct","open","ord","pow","print","property","quit","range","repr","reversed","round","set","setattr","slice","sorted","staticmethod","str","sum","super","tuple","type","vars","zip","__builtins__","__cached__","__doc__","__file__","__loader__","__name__","__package__","__spec__"]
+
+
+def filter_arrays(hashed_array, functions, filename):
+    with open(filename, 'r') as file:
+        file_content = file.read()
+
+    filtered1 = []
+    filtered2 = []
+    
+
+    for value1, value2 in zip(hashed_array, functions):
+        if value1 in file_content:
+            filtered1.append(value1)
+            filtered2.append(value2)
+
+    return filtered1, filtered2
+
+filename = "solve.txt"
+
+filtered1, filtered2 = filter_arrays(hashed_array, functions, filename)
+
+
+print("Filtered Array 1:", filtered1)
+print("length",len(filtered1))
+print("Filtered Array 2:", filtered2)
+print("length",len(filtered2))
