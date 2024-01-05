612b8c53108d165b6395995965063a477685f819
fodhil-ben <benhibafodhil@gmail.com>
2024-01-05 10:07:37 +0100

    edit web chall

--- web/headers_hunter/challenge/app.js
+++ web/headers_hunter/challenge/app.js
@@ -6,19 +6,19 @@ app.get('/', (req, res) => {
 });
 app.post('/', (req, res) => {
     if (req.headers['user-agent'] === 'l33t_br0w$3r') {
-        if (req.headers['authorization'] === 'authoride') {
+        if (req.headers['authorization'] === 'authorised') {
             if (req.headers['content-language'] === 'arabic') {
                 if (req.headers['location'] === 'alphabit') {
                     res.send('Alphabit{GG_Y0u_kn0w_HTTP_headers_y0u_4r3_7h3_b3s7_h34d3rs_hun73r}')
                 }
                 else {
-                    res.send('to get the flag you must be in alphabit')
+                    res.send('to get the flag your location must be in alphabit :) ')
                 }
             } else {
-                res.send('You must talk arabic')
+                res.send('the content must be in arabic')
             }
         } else {
-            res.send('You must be authoride to get the flag')
+            res.send('You must be authorised to get the flag')
         }
     } else {
         res.send('only users that uses our l33t_br0w$3r can access our leet website')
