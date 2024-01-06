f2d159dd26e79e06cf53336081f8ba774e8ffb5f
HartiDhayaa <58786265+dhiaae@users.noreply.github.com>
2024-01-06 01:12:52 +0100

    Add files via upload

---
+++ web/Skill_Hunter_1/src/upload.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+
+if (isset($_FILES['file'])) {
+    if ($_FILES['file']['size'] < 5000) {
+        if (isset($_SESSION['id'])) 
+            $random = $_SESSION['id'];
+        else {
+            $random = bin2hex(random_bytes(16));
+            $_SESSION['id'] = $random;
+        }
+
+        $dir = 'uploads/' . $random; 
+        if (!is_dir($dir)) 
+            mkdir($dir, 0777, true);
+
+        $name = $_FILES['file']['name'];
+        $temp_path = $_FILES['file']['tmp_name'];
+        $path = $dir .'/'. $name;
+        if (move_uploaded_file($temp_path, $path)) {
+            echo '<!DOCTYPE html><html>
+<head>
+<title>Skill Hunter</title>
+</head>
+<body>
+<div class="align-middle well">
+<div class="container my-5 px-4">
+    <h3>Thief book</h3>
+    <p>the NEN ability is successfully stolen... You can find it at this page <a href="'. $path . '">here</a></p>
+</div>
+</div>
+</body>
+</html> ';
+        }
+    } else {
+        die("ability is too large, the book is meant for small abilities only");
+    }
+}
+else 
+    echo "No file sent";
+
+
+?>
+
+ 
+
+
